0a07f96acb8c08097d086dc65adc5abd813dbad7
leonamagaya <leonamagaya@gmail.com>
2023-06-25 07:11:19 -0700

    _printf

---
+++ _printf.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * _printf -  function that produces output according
+ *            to a format.
+ *
+ * @format: input string.
+ *
+ * Return: number of characters.
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int num_char = 0, index, c;
+	char *s;
+
+	va_start(args, format);
+	for (index = 0; format[index] != '\0'; index++)
+	{
+		if (format[index] == '%')
+		{
+			index++;
+			if (format[index] == 'c')
+			{
+				c = va_arg(args, int);
+				_putchar(c);
+				num_char++;
+			}
+			else if (format[index] == 's')
+			{
+				s = va_arg(args, char *);
+				while (*s != '\0')
+				{
+					_putchar(*s);
+					num_char++;
+					s++;
+				}
+			}
+			else if (format[index] == '%')
+			{
+				_putchar('%');
+				num_char++;
+			}
+		}
+		else
+		{
+			_putchar(format[index]);
+			num_char++;
+		}
+	}
+	va_end(args);
+	return (num_char);
+}
---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character written to stdout
+ *
+ * Return: 1 (Success)
+ *        -1 on error and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
