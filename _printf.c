19474d5d4ba686ee096e6a0bcd7d23205455f679
chux <chuxoguh@gmail.com>
2023-06-26 11:02:59 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,19 +1,50 @@
 #include "main.h"
+
 /**
- * _printf -  function that produces output according
- *            to a format.
- * @format: input string.
- * Return: number of characters.
+ * print_character - Prints a single character.
+ * @c: The character to print.
+ * Return: The number of characters printed.
+ */
+int print_character(char c)
+{
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * print_string - Prints a string.
+ * @s: The string to print.
+ * Return: The number of characters printed.
+ */
+int print_string(char *s)
+{
+	int count = 0;
+
+	if (s == NULL)
+		s = "(null)";
+
+	while (*s)
+	{
+		_putchar(*s);
+		count++;
+		s++;
+	}
+	return (count);
+}
+
+/**
+ * _printf - Prints output according to a format.
+ * @format: Input format string.
+ * Return: Number of characters printed.
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int num_char = 0, index, c;
+	int index, num_char = 0;
 	char *s;
 
 	if (format == NULL)
 		return (-1);
-
 	va_start(args, format);
 	for (index = 0; format[index] != '\0'; index++)
 	{
@@ -22,34 +53,30 @@ int _printf(const char *format, ...)
 			index++;
 			if (format[index] == 'c')
 			{
-				c = va_arg(args, int);
-				_putchar(c);
-				num_char++;
+				int c = va_arg(args, int);
+
+				num_char += print_character(c);
 			}
 			else if (format[index] == 's')
 			{
 				s = va_arg(args, char *);
-				if (s == NULL)
-					s = "(null)";
-				for (; *s; s++)
-				{
-					_putchar(*s);
-					num_char++;
-				}
-
+				num_char += print_string(s);
 			}
 			else if (format[index] == '%')
 			{
+				num_char += print_character('%');
+			}
+			else
+			{
 				_putchar('%');
-				num_char++;
+				_putchar(format[index]);
+				num_char += 2;
 			}
 		}
 		else
 		{
-			_putchar(format[index]);
-			num_char++;
+			num_char += print_character(format[index]);
 		}
-	}
-	va_end(args);
+	} va_end(args);
 	return (num_char);
 }
